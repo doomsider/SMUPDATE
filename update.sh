6eb00cdf6a7ced5bad0701cbf2934bae77e35f2f
Reed Harding (AKA Doomsider) <reedharding@gmail.com>
2015-07-28 13:51:50 -0600

    Testing update.sh
    General testing and then I will add a little more robustness

---
+++ update.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# Starmade Selective upgrade script for BASH.  Please run in the parent or root directory of Starmade
+# Requires curl to be installed
+# Grab the releaseindex as a very long string variable
+if command -v curl >/dev/null
+then
+	echo "Curl was found"
+	releaseindex=$(curl http://files.star-made.org/releasebuildindex)
+# Go to the end of the variable at the last .
+	newversion=${releaseindex##*.}
+	echo "newversion $newversion"
+# Get the first version to compare
+	cutstring=${newversion#*_}
+	NEWSMVERSION1=${cutstring%_*}
+	echo "NEWSMVERSION1 $NEWSMVERSION1"
+# Get the second version to compare
+	NEWSMVERSION2=${cutstring#*_}
+	echo "NEWSMVERSION2 $NEWSMVERSION2"
+# Gather the old version from the version.txt file in StarMade
+	oldversion=$(cat StarMade/version.txt)
+	if [ -z "$OLDCKSUMSTRING" ]
+	then
+		echo "No install found for StarMade"
+		OLDSMVER1=0
+		OLDSMVER2=0
+	else
+		echo "oldversion $oldversion"
+	# Get the first version to compare
+		cutstring=${oldversion#*#}
+		OLDSMVER1=${cutstring%_*}
+		echo "OLDSMVER1 $OLDSMVER1"
+	# Get the second version to compare
+		OLDSMVER2=${cutstring#*_}
+		echo "OLDSMVER2 $OLDSMVER2"
+	fi
+# If the first or second newversion exceeds the first or second old version
+	if [ "$NEWSMVERSION1" -gt "$OLDSMVER1" ] || [ "$NEWSMVERSION2" -gt "$OLDSMVER2" ]
+	then 
+		echo "Newer Version Detected"
+# Set the field seperator to new line and then store the chucksums as an array with each element being a line
+		OLD_IFS=$IFS
+		IFS=$'\n'
+		releaseindex=( $(curl http://files.star-made.org$newversion/checksums) )
+		IFS=$OLD_IFS
+# Set line count to 0 then go through the array line by line until the array index is unset		
+		LINECOUNT=0
+		while [ -n "${releaseindex[$LINECOUNT]+set}" ] 
+		do
+			CURRENTSTRING=${releaseindex[$LINECOUNT]}
+			#echo $CURRENTSTRING
+# Format the current line by removing everything after the first space and then removing ./ in the beginning of the name		
+			cutstring=${CURRENTSTRING%[[:space:]]*}
+			cutstring=${cutstring%[[:space:]]*}
+			CURRENTFILE=${cutstring/.\//}
+# Formatfile was added here because when requesting web URL space should be replaced with %20
+			FORMATFILE=${CURRENTFILE// /%20}
+			cutstring=${CURRENTSTRING%[[:space:]]*}
+			cutstring=${cutstring##*[[:space:]]}
+			CURRENTCKSUM=${cutstring%%[[:space:]]*}
+			OLDCKSUMSTRING=$(cksum "StarMade/$CURRENTFILE")
+# Check to see if OLDCKSUMSTRING is set, if not this indicates the file does not exist
+			if [ -z "$OLDCKSUMSTRING" ]
+			then
+				echo "No existing file found - downloading file"
+# Makes sure directory structure is created if it does not exist for the file write
+				if [ ! -f "StarMade/$CURRENTFILE" ]; then
+					mkdir -p "StarMade/$CURRENTFILE"
+					rm -r "StarMade/$CURRENTFILE"
+				fi
+				curl "http://files.star-made.org$newversion/$FORMATFILE" > "StarMade/$CURRENTFILE"
+			else
+				cutstring=${OLDCKSUMSTRING#*[[:space:]]}
+				OLDCKSUM=${cutstring%%[[:space:]]*}
+				echo "CURRENTFILE $CURRENTFILE CURRENTCKSUM $CURRENTCKSUM OLDCKSUM $OLDCKSUM"
+# Check to see if the cksums differ
+				if [ "$CURRENTCKSUM" -ne "$OLDCKSUM" ]
+				then
+# Download the new file and then copy it into the proper location
+					echo "Updated file detected - downloading file"
+# Makes sure directory structure is created if it does not exist for the file write
+					if [ ! -f "StarMade/$CURRENTFILE" ]; then
+						mkdir -p "StarMade/$CURRENTFILE"
+						rm -r "StarMade/$CURRENTFILE"
+					fi
+					curl "http://files.star-made.org$newversion/$FORMATFILE" > "StarMade/$CURRENTFILE"
+				else
+					echo "Current file detected - no action"
+				fi
+			fi
+			let LINECOUNT++
+		done
+	else
+		echo "Version Current"
+	fi
+else
+	echo "You must install Curl to run this"
+fi
